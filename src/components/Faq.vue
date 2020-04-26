aca7a3a6fe3ed54cdffa952bdc6838f1ea296b96
taha ansari <taha.m.ansari@gmail.com>
2020-04-26 16:25:46 +0530

    initial commit

---
+++ src/components/Faq.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="faq">
+      <h1>This is a faq component</h1>    
+  </div>
+</template>
+
+<script>
+import axios from 'axios'; 
+export default {
+  name: 'Faq'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import "../mq";
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+      <h1>This is a home component</h1>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import "../mq";
+</style>
